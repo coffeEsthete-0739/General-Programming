434d2acb849614c002cc422aa23bf1cca569e945
Shakib Ahmed Numan <135098240+coffeEsthete-0739@users.noreply.github.com>
2025-08-17 21:58:58 +0600

    7.Floyd-Warshall_Algorithm.cpp

--- 7.Floyd-Warshall_Algorithm.cpp
+++ 7.Floyd-Warshall_Algorithm.cpp
@@ -3,14 +3,13 @@ using namespace std;
 #define INF 99999
 
 int main(){
-    int n=4;
-    int graph[4][4] = {
-        {0,5,INF,10},
-        {INF,0,3,INF},
-        {INF,INF,0,1},
-        {INF,INF,INF,0}
-    };
-    int dist[4][4];
+    int n;
+    cout << "Enter number of vertices: ";
+    cin >> n;
+    int graph[n][n];
+    cout << "Enter adjacency matrix (use " << INF << " for no edge):\n";
+    for(int i=0;i<n;i++) for(int j=0;j<n;j++) cin >> graph[i][j];
+    int dist[n][n];
     for(int i=0;i<n;i++) for(int j=0;j<n;j++) dist[i][j]=graph[i][j];
 
     for(int k=0;k<n;k++)
