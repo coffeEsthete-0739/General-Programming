5f70fe7b47b9f772f2d485c8aa0149b1c5234e06
Shakib Ahmed Numan <135098240+coffeEsthete-0739@users.noreply.github.com>
2025-08-17 21:41:05 +0600

    8.4-Queens.cpp

---
+++ 8.4-Queens.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool isSafe(int board[], int row, int col){
+    for(int i=0;i<row;i++)
+        if(board[i]==col || abs(board[i]-col)==abs(i-row))
+            return false;
+    return true;
+}
+
+void solve(int board[], int row, int n){
+    if(row==n){
+        for(int i=0;i<n;i++) cout << board[i] << " ";
+        cout << endl;
+        return;
+    }
+    for(int col=0;col<n;col++){
+        if(isSafe(board,row,col)){
+            board[row]=col;
+            solve(board,row+1,n);
+        }
+    }
+}
+
+int main(){
+    int board[4];
+    solve(board,0,4);
+}
