2743ca5c2992174bcc557b2797fa9bf5ef5fc15a
Shakib Ahmed Numan <135098240+coffeEsthete-0739@users.noreply.github.com>
2025-08-17 22:01:26 +0600

    10.Graph_Coloring.cpp

--- 10.Graph_Coloring.cpp
+++ 10.Graph_Coloring.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-bool isSafe(int graph[4][4], int color[], int node, int c, int n){
+bool isSafe(int graph[10][10], int color[], int node, int c, int n){
     for(int i=0;i<n;i++)
         if(graph[node][i] && color[i]==c) return false;
     return true;
 }
 
-bool solve(int graph[4][4], int m, int color[], int node, int n){
+bool solve(int graph[10][10], int m, int color[], int node, int n){
     if(node==n){
         for(int i=0;i<n;i++) cout << color[i] << " ";
         cout << endl;
@@ -24,7 +24,14 @@ bool solve(int graph[4][4], int m, int color[], int node, int n){
 }
 
 int main(){
-    int graph[4][4]={{0,1,1,1},{1,0,1,0},{1,1,0,1},{1,0,1,0}};
-    int color[4]={0};
-    solve(graph,3,color,0,4);
+    int n,m;
+    cout << "Enter number of vertices: ";
+    cin >> n;
+    int graph[10][10];
+    cout << "Enter adjacency matrix:\n";
+    for(int i=0;i<n;i++) for(int j=0;j<n;j++) cin >> graph[i][j];
+    cout << "Enter number of colors: ";
+    cin >> m;
+    int color[10]={0};
+    if(!solve(graph,m,color,0,n)) cout << "No solution!";
 }
